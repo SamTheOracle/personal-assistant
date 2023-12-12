ca68a8a56e34e39232bf78a9e3b6b91d31f01035
Giacomo Zanotti <gzano93@gmail.com>
2023-12-12 22:18:10 +0100

    script modified

--- build-push-docker.sh
+++ build-push-docker.sh
@@ -1,3 +1,4 @@
+version=$1
 mvn clean package
-docker build -f src/main/docker/Dockerfile.jvm . -t oracolo/personal-assistant
-docker push oracolo/personal-assistant
+docker build -f src/main/docker/Dockerfile.jvm . -t oracolo/personal-assistant:"$version"
+docker push oracolo/personal-assistant:"$version"
